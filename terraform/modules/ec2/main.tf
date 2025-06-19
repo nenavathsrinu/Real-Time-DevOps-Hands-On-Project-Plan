6873ba56f88cd1342590f23410dee0e5820d6c75
srinu <nenavath2013@gmail.com>
2025-06-19 17:29:51 +0530

    dev

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,3 +1,13 @@
+locals {
+  combined_user_data = <<-EOF
+    #!/bin/bash
+    set -e
+    ${file("${path.module}/ansible_install.sh")}
+    ${file("${path.module}/install_ansiable_amazon_linux.sh")}
+    ${file("${path.module}/install-maven-kubectl.sh")}
+  EOF
+}
+
 resource "aws_instance" "jenkins" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
@@ -6,7 +16,7 @@ resource "aws_instance" "jenkins" {
   vpc_security_group_ids      = [aws_security_group.jenkins_sg.id]
   associate_public_ip_address = true
   iam_instance_profile        = var.iam_instance_profile
-
+  user_data                   = local.combined_user_data
   tags = {
     Name = var.instance_name
   }
