de461590a80e76372951416fb4b50bcddce730fb
srinu <nenavath2013@gmail.com>
2025-06-20 13:49:58 +0530

    dev

--- terraform/modules/ec2/sg.tf
+++ terraform/modules/ec2/sg.tf
@@ -18,6 +18,14 @@ resource "aws_security_group" "jenkins_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress = {
+    description = "nodejs-port"
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
