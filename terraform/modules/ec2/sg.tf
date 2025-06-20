48a316fdce8b429a380cb5c6586cde020a19ce30
srinu <nenavath2013@gmail.com>
2025-06-20 13:52:38 +0530

    dev

--- terraform/modules/ec2/sg.tf
+++ terraform/modules/ec2/sg.tf
@@ -13,20 +13,12 @@ resource "aws_security_group" "jenkins_sg" {
 
   ingress {
     description = "Allow Jenkins Web UI"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
-  ingress = {
-    description = "nodejs-port"
-    from_port = 3000
-    to_port = 3000
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+   
   egress {
     from_port   = 0
     to_port     = 0
